8d7a41198b93ec69fb2ff2799af6f7f764e70c33
Dalois-30 <claudetapi@yahoo.com>
2023-01-14 21:13:23 +0100

    change url back

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://13.37.224.29:5000/', {
+    const response = await fetch('https://codex.dalois-30.repl.co/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
